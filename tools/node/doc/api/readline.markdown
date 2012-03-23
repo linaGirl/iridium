b0f6fa7992dcaaf99de7eac360dd5b724fc77b4d
michael <michael@joinbox.com>
2012-03-23 14:04:44 +0100

    added node as fake submodule

---
+++ tools/node/doc/api/readline.markdown
@@ -0,0 +1,212 @@
+# Readline
+
+    Stability: 3 - Stable
+
+To use this module, do `require('readline')`. Readline allows reading of a
+stream (such as STDIN) on a line-by-line basis.
+
+Note that once you've invoked this module, your node program will not
+terminate until you've paused the interface. Here's how to allow your
+program to gracefully pause:
+
+    var rl = require('readline');
+
+    var i = rl.createInterface(process.stdin, process.stdout, null);
+    i.question("What do you think of node.js?", function(answer) {
+      // TODO: Log the answer in a database
+      console.log("Thank you for your valuable feedback.");
+
+      i.pause();
+    });
+
+## rl.createInterface(input, output, completer)
+
+Takes two streams and creates a readline interface. The `completer` function
+is used for autocompletion. When given a substring, it returns `[[substr1,
+substr2, ...], originalsubstring]`.
+
+Also `completer` can be run in async mode if it accepts two arguments:
+
+  function completer(linePartial, callback) {
+    callback(null, [['123'], linePartial]);
+  }
+
+`createInterface` is commonly used with `process.stdin` and
+`process.stdout` in order to accept user input:
+
+    var readline = require('readline'),
+      rl = readline.createInterface(process.stdin, process.stdout);
+
+## Class: Interface
+
+The class that represents a readline interface with a stdin and stdout
+stream.
+
+### rl.setPrompt(prompt, length)
+
+Sets the prompt, for example when you run `node` on the command line, you see
+`> `, which is node's prompt.
+
+### rl.prompt()
+
+Readies readline for input from the user, putting the current `setPrompt`
+options on a new line, giving the user a new spot to write.
+
+This will also resume the `in` stream used with `createInterface` if it has
+been paused.
+
+### rl.question(query, callback)
+
+Prepends the prompt with `query` and invokes `callback` with the user's
+response. Displays the query to the user, and then invokes `callback` with the
+user's response after it has been typed.
+
+This will also resume the `in` stream used with `createInterface` if it has
+been paused.
+
+Example usage:
+
+    interface.question('What is your favorite food?', function(answer) {
+      console.log('Oh, so your favorite food is ' + answer);
+    });
+
+### rl.pause()
+
+Pauses the readline `in` stream, allowing it to be resumed later if needed.
+
+### rl.resume()
+
+Resumes the readline `in` stream.
+
+### rl.write()
+
+Writes to tty.
+
+This will also resume the `in` stream used with `createInterface` if it has
+been paused.
+
+### Event: 'line'
+
+`function (line) {}`
+
+Emitted whenever the `in` stream receives a `\n`, usually received when the
+user hits enter, or return. This is a good hook to listen for user input.
+
+Example of listening for `line`:
+
+    rl.on('line', function (cmd) {
+      console.log('You just typed: '+cmd);
+    });
+
+### Event: 'pause'
+
+`function () {}`
+
+Emitted whenever the `in` stream is paused or receives `^D`, respectively known
+as `EOT`. This event is also called if there is no `SIGINT` event listener
+present when the `in` stream receives a `^C`, respectively known as `SIGINT`.
+
+Also emitted whenever the `in` stream is not paused and receives the `SIGCONT`
+event. (See events `SIGTSTP` and `SIGCONT`)
+
+Example of listening for `pause`:
+
+    rl.on('pause', function() {
+      console.log('Readline paused.');
+    });
+
+### Event: 'resume'
+
+`function () {}`
+
+Emitted whenever the `in` stream is resumed.
+
+Example of listening for `resume`:
+
+    rl.on('resume', function() {
+      console.log('Readline resumed.');
+    });
+
+### Event: 'SIGINT'
+
+`function () {}`
+
+Emitted whenever the `in` stream receives a `^C`, respectively known as
+`SIGINT`. If there is no `SIGINT` event listener present when the `in` stream
+receives a `SIGINT`, `pause` will be triggered.
+
+Example of listening for `SIGINT`:
+
+    rl.on('SIGINT', function() {
+      rl.question('Are you sure you want to exit?', function(answer) {
+        if (answer.match(/^y(es)?$/i)) rl.pause();
+      });
+    });
+
+### Event: 'SIGTSTP'
+
+`function () {}`
+
+**This does not work on Windows.**
+
+Emitted whenever the `in` stream receives a `^Z`, respectively known as
+`SIGTSTP`. If there is no `SIGTSTP` event listener present when the `in` stream
+receives a `SIGTSTP`, the program will be sent to the background.
+
+When the program is resumed with `fg`, the `pause` and `SIGCONT` events will be
+emitted. You can use either to resume the stream.
+
+The `pause` and `SIGCONT` events will not be triggered if the stream was paused
+before the program was sent to the background.
+
+Example of listening for `SIGTSTP`:
+
+    rl.on('SIGTSTP', function() {
+      // This will override SIGTSTP and prevent the program from going to the
+      // background.
+      console.log('Caught SIGTSTP.');
+    });
+
+### Event: 'SIGCONT'
+
+`function () {}`
+
+**This does not work on Windows.**
+
+Emitted whenever the `in` stream is sent to the background with `^Z`,
+respectively known as `SIGTSTP`, and then continued with `fg`. This event only
+emits if the stream was not paused before sending the program to the
+background.
+
+Example of listening for `SIGCONT`:
+
+    rl.on('SIGCONT', function() {
+      // `prompt` will automatically resume the stream
+      rl.prompt();
+    });
+
+
+Here's an example of how to use all these together to craft a tiny command
+line interface:
+
+    var readline = require('readline'),
+        rl = readline.createInterface(process.stdin, process.stdout);
+
+    rl.setPrompt('OHAI> ');
+    rl.prompt();
+
+    rl.on('line', function(line) {
+      switch(line.trim()) {
+        case 'hello':
+          console.log('world!');
+          break;
+        default:
+          console.log('Say what? I might have heard `' + line.trim() + '`');
+          break;
+      }
+      rl.prompt();
+    }).on('pause', function() {
+      console.log('Have a great day!');
+      process.exit(0);
+    });
+
---
+++ tools/node/lib/readline.js
@@ -0,0 +1,725 @@
+// Copyright Joyent, Inc. and other Node contributors.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to permit
+// persons to whom the Software is furnished to do so, subject to the
+// following conditions:
+//
+// The above copyright notice and this permission notice shall be included
+// in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN
+// NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+// OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
+// USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+// Inspiration for this code comes from Salvatore Sanfilippo's linenoise.
+// https://github.com/antirez/linenoise
+// Reference:
+// * http://invisible-island.net/xterm/ctlseqs/ctlseqs.html
+// * http://www.3waylabs.com/nw/WWW/products/wizcon/vt220.html
+
+var kHistorySize = 30;
+var kBufSize = 10 * 1024;
+
+var util = require('util');
+var inherits = require('util').inherits;
+var EventEmitter = require('events').EventEmitter;
+var tty = require('tty');
+
+
+exports.createInterface = function(input, output, completer) {
+  return new Interface(input, output, completer);
+};
+
+
+function Interface(input, output, completer) {
+  if (!(this instanceof Interface)) {
+    return new Interface(input, output, completer);
+  }
+  EventEmitter.call(this);
+
+  completer = completer || function() { return []; };
+
+  if (typeof completer !== 'function') {
+    throw new TypeError('Argument \'completer\' must be a function');
+  }
+
+  var self = this;
+
+  this.output = output;
+  this.input = input;
+  input.resume();
+
+  // Check arity, 2 - for async, 1 for sync
+  this.completer = completer.length === 2 ? completer : function(v, callback) {
+    callback(null, completer(v));
+  };
+
+  this.setPrompt('> ');
+
+  this.enabled = output.isTTY;
+
+  if (parseInt(process.env['NODE_NO_READLINE'], 10)) {
+    this.enabled = false;
+  }
+
+  if (!this.enabled) {
+    input.on('data', function(data) {
+      self._normalWrite(data);
+    });
+
+  } else {
+
+    // input usually refers to stdin
+    input.on('keypress', function(s, key) {
+      self._ttyWrite(s, key);
+    });
+
+    // Current line
+    this.line = '';
+
+    // Check process.env.TERM ?
+    tty.setRawMode(true);
+    this.enabled = true;
+
+    // Cursor position on the line.
+    this.cursor = 0;
+
+    this.history = [];
+    this.historyIndex = -1;
+
+    var winSize = output.getWindowSize();
+    exports.columns = winSize[0];
+
+    if (process.listeners('SIGWINCH').length === 0) {
+      process.on('SIGWINCH', function() {
+        var winSize = output.getWindowSize();
+        exports.columns = winSize[0];
+
+        // FIXME: when #2922 will be approved, change this to
+        // output.on('resize', ...
+        self._refreshLine();
+      });
+    }
+  }
+}
+
+inherits(Interface, EventEmitter);
+
+Interface.prototype.__defineGetter__('columns', function() {
+  return exports.columns;
+});
+
+Interface.prototype.setPrompt = function(prompt, length) {
+  this._prompt = prompt;
+  if (length) {
+    this._promptLength = length;
+  } else {
+    var lines = prompt.split(/[\r\n]/);
+    var lastLine = lines[lines.length - 1];
+    this._promptLength = Buffer.byteLength(lastLine);
+  }
+};
+
+
+Interface.prototype.prompt = function(preserveCursor) {
+  if (this.paused) this.resume();
+  if (this.enabled) {
+    if (!preserveCursor) this.cursor = 0;
+    this._refreshLine();
+  } else {
+    this.output.write(this._prompt);
+  }
+};
+
+
+Interface.prototype.question = function(query, cb) {
+  if (typeof cb === 'function') {
+    if (this._questionCallback) {
+      this.prompt();
+    } else {
+      this._oldPrompt = this._prompt;
+      this.setPrompt(query);
+      this._questionCallback = cb;
+      this.prompt();
+    }
+  }
+};
+
+
+Interface.prototype._onLine = function(line) {
+  if (this._questionCallback) {
+    var cb = this._questionCallback;
+    this._questionCallback = null;
+    this.setPrompt(this._oldPrompt);
+    cb(line);
+  } else {
+    this.emit('line', line);
+  }
+};
+
+
+Interface.prototype._addHistory = function() {
+  if (this.line.length === 0) return '';
+
+  this.history.unshift(this.line);
+  this.historyIndex = -1;
+
+  // Only store so many
+  if (this.history.length > kHistorySize) this.history.pop();
+
+  return this.history[0];
+};
+
+
+Interface.prototype._refreshLine = function() {
+  var columns = this.columns;
+
+  // line length
+  var line = this._prompt + this.line;
+  var lineLength = line.length;
+  var lineCols = lineLength % columns;
+  var lineRows = (lineLength - lineCols) / columns;
+
+  // cursor position
+  var cursorPos = this._getCursorPos();
+
+  // first move to the bottom of the current line, based on cursor pos
+  var prevRows = this.prevRows || 0;
+  if (prevRows > 0) {
+    this.output.moveCursor(0, -prevRows);
+  }
+
+  // Cursor to left edge.
+  this.output.cursorTo(0);
+  // erase data
+  this.output.clearScreenDown();
+
+  // Write the prompt and the current buffer content.
+  this.output.write(line);
+
+  // Force terminal to allocate a new line
+  if (lineCols === 0) {
+    this.output.write(' ');
+  }
+
+  // Move cursor to original position.
+  this.output.cursorTo(cursorPos.cols);
+
+  var diff = lineRows - cursorPos.rows;
+  if (diff > 0) {
+    this.output.moveCursor(0, -diff);
+  }
+
+  this.prevRows = cursorPos.rows;
+};
+
+
+Interface.prototype.pause = function() {
+  if (this.paused) return;
+  if (this.enabled) {
+    tty.setRawMode(false);
+  }
+  this.input.pause();
+  this.paused = true;
+  this.emit('pause');
+};
+
+
+Interface.prototype.resume = function() {
+  this.input.resume();
+  if (this.enabled) {
+    tty.setRawMode(true);
+  }
+  this.paused = false;
+  this.emit('resume');
+};
+
+
+Interface.prototype.write = function(d, key) {
+  if (this.paused) this.resume();
+  this.enabled ? this._ttyWrite(d, key) : this._normalWrite(d, key);
+};
+
+
+Interface.prototype._normalWrite = function(b) {
+  // Very simple implementation right now. Should try to break on
+  // new lines.
+  if (b !== undefined)
+    this._onLine(b.toString());
+};
+
+Interface.prototype._insertString = function(c) {
+  //BUG: Problem when adding tabs with following content.
+  //     Perhaps the bug is in _refreshLine(). Not sure.
+  //     A hack would be to insert spaces instead of literal '\t'.
+  if (this.cursor < this.line.length) {
+    var beg = this.line.slice(0, this.cursor);
+    var end = this.line.slice(this.cursor, this.line.length);
+    this.line = beg + c + end;
+    this.cursor += c.length;
+    this._refreshLine();
+  } else {
+    this.line += c;
+    this.cursor += c.length;
+    this.output.write(c);
+
+    // a hack to get the line refreshed if it's needed
+    this._moveCursor(0);
+  }
+};
+
+Interface.prototype._tabComplete = function() {
+  var self = this;
+
+  self.pause();
+  self.completer(self.line.slice(0, self.cursor), function(err, rv) {
+    self.resume();
+
+    if (err) {
+      // XXX Log it somewhere?
+      return;
+    }
+
+    var completions = rv[0],
+        completeOn = rv[1];  // the text that was completed
+    if (completions && completions.length) {
+      // Apply/show completions.
+      if (completions.length === 1) {
+        self._insertString(completions[0].slice(completeOn.length));
+      } else {
+        self.output.write('\r\n');
+        var width = completions.reduce(function(a, b) {
+          return a.length > b.length ? a : b;
+        }).length + 2;  // 2 space padding
+        var maxColumns = Math.floor(self.columns / width) || 1;
+
+        function handleGroup(group) {
+          if (group.length == 0) {
+            return;
+          }
+          var minRows = Math.ceil(group.length / maxColumns);
+          for (var row = 0; row < minRows; row++) {
+            for (var col = 0; col < maxColumns; col++) {
+              var idx = row * maxColumns + col;
+              if (idx >= group.length) {
+                break;
+              }
+              var item = group[idx];
+              self.output.write(item);
+              if (col < maxColumns - 1) {
+                for (var s = 0, itemLen = item.length; s < width - itemLen;
+                     s++) {
+                  self.output.write(' ');
+                }
+              }
+            }
+            self.output.write('\r\n');
+          }
+          self.output.write('\r\n');
+        }
+
+        var group = [], c;
+        for (var i = 0, compLen = completions.length; i < compLen; i++) {
+          c = completions[i];
+          if (c === '') {
+            handleGroup(group);
+            group = [];
+          } else {
+            group.push(c);
+          }
+        }
+        handleGroup(group);
+
+        // If there is a common prefix to all matches, then apply that
+        // portion.
+        var f = completions.filter(function(e) { if (e) return e; });
+        var prefix = commonPrefix(f);
+        if (prefix.length > completeOn.length) {
+          self._insertString(prefix.slice(completeOn.length));
+        }
+
+      }
+      self._refreshLine();
+    }
+  });
+};
+
+
+function commonPrefix(strings) {
+  if (!strings || strings.length == 0) {
+    return '';
+  }
+  var sorted = strings.slice().sort();
+  var min = sorted[0];
+  var max = sorted[sorted.length - 1];
+  for (var i = 0, len = min.length; i < len; i++) {
+    if (min[i] != max[i]) {
+      return min.slice(0, i);
+    }
+  }
+  return min;
+}
+
+
+Interface.prototype._wordLeft = function() {
+  if (this.cursor > 0) {
+    var leading = this.line.slice(0, this.cursor);
+    var match = leading.match(/([^\w\s]+|\w+|)\s*$/);
+    this._moveCursor(-match[0].length);
+  }
+};
+
+
+Interface.prototype._wordRight = function() {
+  if (this.cursor < this.line.length) {
+    var trailing = this.line.slice(this.cursor);
+    var match = trailing.match(/^(\s+|\W+|\w+)\s*/);
+    this._moveCursor(match[0].length);
+  }
+};
+
+
+Interface.prototype._deleteLeft = function() {
+  if (this.cursor > 0 && this.line.length > 0) {
+    this.line = this.line.slice(0, this.cursor - 1) +
+                this.line.slice(this.cursor, this.line.length);
+
+    this.cursor--;
+    this._refreshLine();
+  }
+};
+
+
+Interface.prototype._deleteRight = function() {
+  this.line = this.line.slice(0, this.cursor) +
+              this.line.slice(this.cursor + 1, this.line.length);
+  this._refreshLine();
+};
+
+
+Interface.prototype._deleteWordLeft = function() {
+  if (this.cursor > 0) {
+    var leading = this.line.slice(0, this.cursor);
+    var match = leading.match(/([^\w\s]+|\w+|)\s*$/);
+    leading = leading.slice(0, leading.length - match[0].length);
+    this.line = leading + this.line.slice(this.cursor, this.line.length);
+    this.cursor = leading.length;
+    this._refreshLine();
+  }
+};
+
+
+Interface.prototype._deleteWordRight = function() {
+  if (this.cursor < this.line.length) {
+    var trailing = this.line.slice(this.cursor);
+    var match = trailing.match(/^(\s+|\W+|\w+)\s*/);
+    this.line = this.line.slice(0, this.cursor) +
+                trailing.slice(match[0].length);
+    this._refreshLine();
+  }
+};
+
+
+Interface.prototype._deleteLineLeft = function() {
+  this.line = this.line.slice(this.cursor);
+  this.cursor = 0;
+  this._refreshLine();
+};
+
+
+Interface.prototype._deleteLineRight = function() {
+  this.line = this.line.slice(0, this.cursor);
+  this._refreshLine();
+};
+
+
+Interface.prototype.clearLine = function() {
+  this._moveCursor(+Infinity);
+  this.output.write('\r\n');
+  this.line = '';
+  this.cursor = 0;
+  this.prevRows = 0;
+};
+
+
+Interface.prototype._line = function() {
+  var line = this._addHistory();
+  this.clearLine();
+  this._onLine(line);
+};
+
+
+Interface.prototype._historyNext = function() {
+  if (this.historyIndex > 0) {
+    this.historyIndex--;
+    this.line = this.history[this.historyIndex];
+    this.cursor = this.line.length; // set cursor to end of line.
+    this._refreshLine();
+
+  } else if (this.historyIndex === 0) {
+    this.historyIndex = -1;
+    this.cursor = 0;
+    this.line = '';
+    this._refreshLine();
+  }
+};
+
+
+Interface.prototype._historyPrev = function() {
+  if (this.historyIndex + 1 < this.history.length) {
+    this.historyIndex++;
+    this.line = this.history[this.historyIndex];
+    this.cursor = this.line.length; // set cursor to end of line.
+
+    this._refreshLine();
+  }
+};
+
+
+// Returns current cursor's position and line
+Interface.prototype._getCursorPos = function() {
+  var columns = this.columns;
+  var cursorPos = this.cursor + this._promptLength;
+  var cols = cursorPos % columns;
+  var rows = (cursorPos - cols) / columns;
+  return {cols: cols, rows: rows};
+};
+
+
+// This function moves cursor dx places to the right
+// (-dx for left) and refreshes the line if it is needed
+Interface.prototype._moveCursor = function(dx) {
+  var oldcursor = this.cursor;
+  var oldPos = this._getCursorPos();
+  this.cursor += dx;
+
+  // bounds check
+  if (this.cursor < 0) this.cursor = 0;
+  if (this.cursor > this.line.length) this.cursor = this.line.length;
+
+  var newPos = this._getCursorPos();
+
+  // check if cursors are in the same line
+  if (oldPos.rows == newPos.rows && newPos.cols != 0) {
+    this.output.moveCursor(this.cursor - oldcursor, 0);
+    this.prevRows = newPos.rows;
+  } else {
+    this._refreshLine();
+  }
+};
+
+
+// handle a write from the tty
+Interface.prototype._ttyWrite = function(s, key) {
+  var next_word, next_non_word, previous_word, previous_non_word;
+  key = key || {};
+
+  // Ignore escape key - Fixes #2876
+  if (key.name == 'escape') return;
+
+  if (key.ctrl && key.shift) {
+    /* Control and shift pressed */
+    switch (key.name) {
+      case 'backspace':
+        this._deleteLineLeft();
+        break;
+
+      case 'delete':
+        this._deleteLineRight();
+        break;
+    }
+
+  } else if (key.ctrl) {
+    /* Control key pressed */
+
+    switch (key.name) {
+      case 'c':
+        if (this.listeners('SIGINT').length) {
+          this.emit('SIGINT');
+        } else {
+          // Pause the stream
+          this.pause();
+        }
+        break;
+
+      case 'h': // delete left
+        this._deleteLeft();
+        break;
+
+      case 'd': // delete right or EOF
+        if (this.cursor === 0 && this.line.length === 0) {
+          this.pause();
+        } else if (this.cursor < this.line.length) {
+          this._deleteRight();
+        }
+        break;
+
+      case 'u': // delete the whole line
+        this.cursor = 0;
+        this.line = '';
+        this._refreshLine();
+        break;
+
+      case 'k': // delete from current to end of line
+        this._deleteLineRight();
+        break;
+
+      case 'a': // go to the start of the line
+        this._moveCursor(-Infinity);
+        break;
+
+      case 'e': // go to the end of the line
+        this._moveCursor(+Infinity);
+        break;
+
+      case 'b': // back one character
+        this._moveCursor(-1);
+        break;
+
+      case 'f': // forward one character
+        this._moveCursor(+1);
+        break;
+
+      case 'n': // next history item
+        this._historyNext();
+        break;
+
+      case 'p': // previous history item
+        this._historyPrev();
+        break;
+
+      case 'z':
+        if (process.platform == 'win32') break;
+        if (this.listeners('SIGTSTP').length) {
+          this.emit('SIGTSTP');
+        } else {
+          process.once('SIGCONT', (function(self) {
+            return function() {
+              // Don't raise events if stream has already been abandoned.
+              if (!self.paused) {
+                // Stream must be paused and resumed after SIGCONT to catch
+                // SIGINT, SIGTSTP, and EOF.
+                self.pause();
+                self.emit('SIGCONT');
+              }
+            };
+          })(this));
+          process.kill(process.pid, 'SIGTSTP');
+        }
+        break;
+
+      case 'w': // delete backwards to a word boundary
+      case 'backspace':
+        this._deleteWordLeft();
+        break;
+
+      case 'delete': // delete forward to a word boundary
+        this._deleteWordRight();
+        break;
+
+      case 'backspace':
+        this._deleteWordLeft();
+        break;
+
+      case 'left':
+        this._wordLeft();
+        break;
+
+      case 'right':
+        this._wordRight();
+        break;
+    }
+
+  } else if (key.meta) {
+    /* Meta key pressed */
+
+    switch (key.name) {
+      case 'b': // backward word
+        this._wordLeft();
+        break;
+
+      case 'f': // forward word
+        this._wordRight();
+        break;
+
+      case 'd': // delete forward word
+      case 'delete':
+        this._deleteWordRight();
+        break;
+
+      case 'backspace': // delete backwards to a word boundary
+        this._deleteWordLeft();
+        break;
+    }
+
+  } else {
+    /* No modifier keys used */
+
+    switch (key.name) {
+      case 'enter':
+        this._line();
+        break;
+
+      case 'backspace':
+        this._deleteLeft();
+        break;
+
+      case 'delete':
+        this._deleteRight();
+        break;
+
+      case 'tab': // tab completion
+        this._tabComplete();
+        break;
+
+      case 'left':
+        this._moveCursor(-1);
+        break;
+
+      case 'right':
+        this._moveCursor(+1);
+        break;
+
+      case 'home':
+        this._moveCursor(-Infinity);
+        break;
+
+      case 'end':
+        this._moveCursor(+Infinity);
+        break;
+
+      case 'up':
+        this._historyPrev();
+        break;
+
+      case 'down':
+        this._historyNext();
+        break;
+
+      default:
+        if (Buffer.isBuffer(s))
+          s = s.toString('utf-8');
+
+        if (s) {
+          var lines = s.split(/\r\n|\n|\r/);
+          for (var i = 0, len = lines.length; i < len; i++) {
+            if (i > 0) {
+              this._line();
+            }
+            this._insertString(lines[i]);
+          }
+        }
+    }
+  }
+};
+
+
+exports.Interface = Interface;
