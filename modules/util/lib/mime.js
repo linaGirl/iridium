b3bb6c80c00a09184c753b4a98ea9e29f66574c4
Michael van der Weg <michael@joinbox.com>
2012-04-12 13:55:52 +0800

    mime module

--- modules/util/lib/mime.js
+++ modules/util/lib/mime.js
@@ -10,25 +10,25 @@
 			, json:			{ defaultCharset: "utf-8", type: "application/json" }
 			, js:			{ defaultCharset: "utf-8", type: "application/javascript" }
 			, jsm:			{ defaultCharset: "utf-8", type: "application/javascript" }
-			, unknown:		{ defaultCharset: "", type: "application/octet-stream" }
-			, bin:			{ defaultCharset: "", type: "application/octet-stream" }
-			, ogg:			{ defaultCharset: "", type: "application/ogg" }
-			, pdf:			{ defaultCharset: "", type: "application/pdf" }
-			, wof:			{ defaultCharset: "", type: "application/x-woff" }
+			, unknown:		{ defaultCharset: "binary", type: "application/octet-stream" }
+			, bin:			{ defaultCharset: "binary", type: "application/octet-stream" }
+			, ogg:			{ defaultCharset: "binary", type: "application/ogg" }
+			, pdf:			{ defaultCharset: "binary", type: "application/pdf" }
+			, wof:			{ defaultCharset: "binary", type: "application/x-woff" }
 			, xhtml:		{ defaultCharset: "utf-8", type: "application/xhtml+xml" }
-			, zip:			{ defaultCharset: "", type: "application/zip" }
-			, mp4:			{ defaultCharset: "", type: "audio/mp4" }
-			, mp3:			{ defaultCharset: "", type: "audio/mpeg" }
-			, wav:			{ defaultCharset: "", type: "audio/vnd.wave" }
-			, webm:			{ defaultCharset: "", type: "audio/webm" }
-			, gif:			{ defaultCharset: "", type: "image/gif" }
-			, jpg:			{ defaultCharset: "", type: "image/jpeg" }
-			, jpeg:			{ defaultCharset: "", type: "image/jpeg" }
-			, png:			{ defaultCharset: "", type: "image/png" }
+			, zip:			{ defaultCharset: "binary", type: "application/zip" }
+			, mp4:			{ defaultCharset: "binary", type: "audio/mp4" }
+			, mp3:			{ defaultCharset: "binary", type: "audio/mpeg" }
+			, wav:			{ defaultCharset: "binary", type: "audio/vnd.wave" }
+			, webm:			{ defaultCharset: "binary", type: "audio/webm" }
+			, gif:			{ defaultCharset: "binary", type: "image/gif" }
+			, jpg:			{ defaultCharset: "binary", type: "image/jpeg" }
+			, jpeg:			{ defaultCharset: "binary", type: "image/jpeg" }
+			, png:			{ defaultCharset: "binary", type: "image/png" }
 			, svg:			{ defaultCharset: "utf-8", type: "image/svg+xml" }
-			, tiff:			{ defaultCharset: "", type: "image/tiff" }
-			, ico:			{ defaultCharset: "", type: "image/vnd.microsoft.icon" }
-			, ttf:			{ defaultCharset: "", type: "application/x-font-ttf" }
+			, tiff:			{ defaultCharset: "binary", type: "image/tiff" }
+			, ico:			{ defaultCharset: "binary", type: "image/vnd.microsoft.icon" }
+			, ttf:			{ defaultCharset: "binary", type: "application/x-font-ttf" }
 			, css:			{ defaultCharset: "utf-8", type: "text/css" }
 			, cssm:			{ defaultCharset: "utf-8", type: "text/css" }
 			, csv:			{ defaultCharset: "utf-8", type: "text/csv" }
@@ -38,10 +38,36 @@
 			, tpl:			{ defaultCharset: "utf-8", type: "text/html" }
 			, tplm:			{ defaultCharset: "utf-8", type: "text/html" }
 			, pong:			{ defaultCharset: "utf-8", type: "text/html" }
+			, _:			{ defaultCharset: "binary", type: "" }
 		}
 
 
 		, getType: function( extension ){
-			return this.__types[ extension ] || { type: "", defaultCharset: "" };
+			if ( this.__types[ extension ] ){
+				return this.__types[ extension ];
+			}
+			else {
+				return this.__types[ "_" ]; 
+			}
+		}
+		
+
+		, isBinary: function( extension ){
+			if ( this.__types[ extension ] ){
+				return this.__types[ extension ].defaultCharset === "binary";
+			}
+			else {
+				return true;
+			}
+		}
+
+
+		, get: function( extension ){
+			if ( this.__types[ extension ] ){
+				return this.__types[ extension ].type + this.__types[ extension ].defaultCharset ? ( "; charset=" + this.__types[ extension ].defaultCharset );
+			}
+			else {
+				return "";
+			}
 		}
 	};
