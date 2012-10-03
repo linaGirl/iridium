c8294f407756b92706a8d7332734ec63cc94f10a
michael <michael@joinbox.com>
2012-10-03 15:03:56 +0200

    other gcm implementation

--- modules/api/index.js
+++ modules/api/index.js
@@ -3,6 +3,6 @@
 
 
 	module.exports = {
-		APNS: require( "./node_modules/apn" )
-		, GCM: require( "./node_modules/node-gcm" )
+		  APNS: require( "./node_modules/apn" )
+		, GCM: require( "./node_modules/gcm/lib/gcm" ).GCM
 	};
