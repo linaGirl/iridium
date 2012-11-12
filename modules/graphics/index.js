f2f96d45fdb4b13adc47b20f35f3d4f33514b1c7
michael <michael@joinbox.com>
2012-11-12 17:31:09 +0100

    removed node-canvas

--- modules/graphics/index.js
+++ modules/graphics/index.js
@@ -3,8 +3,8 @@
 
 
 	module.exports = {
-		Canvas: require( "./node_modules/canvas" )
+		//Canvas: require( "./node_modules/canvas" )
 		//Canvas: require( "./dep/node-canvas" )
-		, gm: require( "./node_modules/gm" )
+		 gm: require( "./node_modules/gm" )
 		, Image: require( "./lib/image" )
 	};
