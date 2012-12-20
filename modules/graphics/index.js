506bed5a1b0ed79226db4804c864668fbbce056c
michael <michael@joinbox.com>
2012-12-20 13:47:44 +0100

    added qr

--- modules/graphics/index.js
+++ modules/graphics/index.js
@@ -5,7 +5,8 @@
 	module.exports = {
 		//Canvas: require( "./node_modules/canvas" )
 		//Canvas: require( "./dep/node-canvas" )
-		  gm: require( "./node_modules/gm" )
-		, Image: require( "./lib/image" )
-		, PDFDocument: require( "pdfkit" )
+		  gm: 			require( "./node_modules/gm" )
+		, Image: 		require( "./lib/image" )
+		, PDFDocument: 	require( "pdfkit" )
+		, QREncoder:	require( "qr" ).Encoder
 	};
